091e6f420160f21989bf49c41fcb07a17f95509d
Victor Jimenez <vic.jicama@gmail.com>
2019-12-21 16:11:26 +0100

    Handle tilde in workspace path

--- start.sh
+++ start.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
-export REPOFLOW_WORKSPACE=./workspace
-echo "REPOFLOW_WORKSPACE=$REPOFLOW_WORKSPACE"
-
-export REPOFLOW_GRAPH_HOME=./node_modules/@nebulario/nodeflow-local-graph
-export REPOFLOW_SERVER_HOME=./node_modules/@nebulario/nodeflow-local-server
-export REPOFLOW_GRAPH_SERVICE_HOST=localhost
-export REPOFLOW_GRAPH_SERVICE_PORT=4079
-
+export REPOFLOW_WORKSPACE=~/repoflow/workspace
+export PLUGIN_REPOFLOW_WORKSPACE=${REPOFLOW_WORKSPACE}
+export REPOFLOW_GRAPHQL_SERVICE_HOST=localhost
+export REPOFLOW_GRAPHQL_SERVICE_PORT=4081
 export REPOFLOW_WEB_SERVICE_HOST=localhost
-export REPOFLOW_WEB_SERVICE_PORT=5079
-export REPOFLOW_GRAPH_URL=http://localhost:${REPOFLOW_GRAPH_SERVICE_PORT}/graphql
-export REPOFLOW_EVENTS_URL=http://localhost:${REPOFLOW_GRAPH_SERVICE_PORT}/events
+export REPOFLOW_WEB_SERVICE_PORT=5081
+export REPOFLOW_PLUGIN_SERVICE_HOST=localhost
+export REPOFLOW_PLUGIN_SERVICE_PORT=7081
 
-echo "Starting graph at port ${REPOFLOW_GRAPH_SERVICE_PORT}"
-node ${REPOFLOW_GRAPH_HOME}/dist/index.js &
+export REPOFLOW_PLUGIN_SERVER=http://${REPOFLOW_PLUGIN_SERVICE_HOST}:${REPOFLOW_PLUGIN_SERVICE_PORT}
+export REPOFLOW_GRAPHQL_SERVER=http://${REPOFLOW_GRAPHQL_SERVICE_HOST}:${REPOFLOW_GRAPHQL_SERVICE_PORT}
+export REPOFLOW_GRAPHQL_URL=${REPOFLOW_GRAPHQL_SERVER}/graphql
+export REPOFLOW_EVENTS_URL=${REPOFLOW_GRAPHQL_SERVER}/events
+export REPOFLOW_PLUGINS_URL=${REPOFLOW_PLUGIN_SERVER}/
+
+export REPOFLOW_GRAPHQL_VERSION=1.73.8-master #module @nebulario/nodeflow-local-graph
+export REPOFLOW_WEB_VERSION=1.71.12-master #module @nebulario/nodeflow-local-server
+export REPOFLOW_PLUGIN_VERSION=1.70.2-master #module @nebulario/repoflow-plugin-server
+
+echo "Starting..."
+echo "{\"dependencies\":{\"@nebulario/nodeflow-local-server\":\"${REPOFLOW_WEB_VERSION}\",\"@nebulario/nodeflow-local-graph\":\"${REPOFLOW_GRAPHQL_VERSION}\",\"@nebulario/repoflow-plugin-server\":\"${REPOFLOW_PLUGIN_VERSION}\"}}" > package.json
+
+
+export NODE_ENV="development"
+yarn install --production=true
+
+echo "Starting graph at port ${REPOFLOW_GRAPHQL_SERVICE_PORT}"
+node ./node_modules/@nebulario/nodeflow-local-graph/dist/index.js &
 echo "Starting web at port ${REPOFLOW_WEB_SERVICE_PORT}"
-node ${REPOFLOW_SERVER_HOME}/dist/index.js &
+node ./node_modules/@nebulario/nodeflow-local-server/dist/index.js &
+echo "Starting plugins at port ${REPOFLOW_PLUGIN_SERVICE_PORT}"
+node ./node_modules/@nebulario/repoflow-plugin-server/dist/index.js &
 
 wait
